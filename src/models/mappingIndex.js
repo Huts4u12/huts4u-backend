dc727c6bb07ffc8be2948eb09ed777e8ba7025d3
pusty1830 <126335408+pusty1830@users.noreply.github.com>
2025-10-03 10:46:24 +0530

    first commit

---
+++ src/models/mappingIndex.js
@@ -0,0 +1,8 @@
+module.exports = {
+  User: require("./user.model"),
+  Hotel: require("./hotel.model"),
+  Room: require("./rooms.models"),
+  Booking: require("./bookings"),
+  Contact: require("./contact.model"),
+  Agreement:require("./agreement"),
+};
